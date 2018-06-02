f95602039440b547eec23d8b69bc93b551e92d5e
Grand Master <gmastrbit@gmail.com>
2018-06-02 17:02:43 +0300

    Delivery optimization
    Delete files
    New desing

--- mypage.php
+++ mypage.php
@@ -4,7 +4,6 @@
 <html lang="uk">
 	<head> 
 		<title> Моя сторінка </title>
-		<!-- Global site tag (gtag.js) - Google Analytics -->
 		<script async src="https://www.googletagmanager.com/gtag/js?id=UA-120229196-2"></script>
 		<script>
 			window.dataLayer = window.dataLayer || [];
@@ -16,8 +15,7 @@
 		<link rel="shortcut icon" href="mypage/img/ico.png" type="image/x-icon">
 		<style>
 			body{ font-family: pt_sans; overflow-x: hidden; }
-			.load-container {
-				background: linear-gradient(140deg, #9575cd, #7e57c2, #673ab7, #5e35b1, #512da8, #4527a0, #311b92);
+			.load-container {			
 				background: #fff;
 				background-size: 1400% 1400%;
 				height: 100%;
@@ -54,7 +52,7 @@
 				position: absolute;
 				top: 50%;
 				text-indent: -9999em;
-			-webkit-animation: load8 1.1s infinite linear;}
+				-webkit-animation: load8 1.1s infinite linear;}
 			.load8 .loader, .load8 .loader:after { border-radius: 50%; height: 8em; width: 8em;} 
 			.c-c{ align-items: center; display: flex; justify-content: center; }
 			.cssload-container { width: 114px; margin: 0 auto; }
@@ -106,11 +104,7 @@
 			.only-mob{ display: none; }
 			@media screen and (max-width:800px) {
 				.project-img, .zoom, .bmark-image{ display: none; }
-				.project-img-mobile{
-				border-radius: 5px;
-				border: 1px solid #D1C4E9;
-				display: flex;
-				width: 100%;}
+				.project-img-mobile{ border-radius: 5px; border: 1px solid #D1C4E9; display: flex; width: 100%; }
 				.only-mob{ display: block; }
 			}
 		</style>
@@ -120,38 +114,22 @@
 	</head>
 	<body id="js-scrollspy">
 	<div class="load-container load8 c-c">
-		<div class="cssload-container">
-		<div class="cssload-circle-1"> <div class="cssload-circle-2"> <div class="cssload-circle-3"> <div class="cssload-circle-4"> <div class="cssload-circle-5"> <div class="cssload-circle-6"> <div class="cssload-circle-7"> <div class="cssload-circle-8">
-		</div> </div> </div> </div> </div> </div> </div> </div> </div> 
+		<div class="cssload-container"> <div class="cssload-circle-1"> <div class="cssload-circle-2"> <div class="cssload-circle-3"> <div class="cssload-circle-4"> <div class="cssload-circle-5"> <div class="cssload-circle-6"> <div class="cssload-circle-7"> <div class="cssload-circle-8">	</div> </div> </div> </div> </div> </div> </div> </div> </div> 
 	</div>
 	<span id="my-head"> </span>
 	<div class="all">
 		<div class="header">
 			<div class="logo">
-				<div class="ico gradient1" data-gradient-css="linear-gradient(45deg, #9575cd, #7e57c2, #673ab7, #5e35b1, #512da8, #4527a0, #311b92)"> G </div> 
-			</div>
-			<div class="ripple"> 
-				<div class="overlay-text"> Богдан Щербаков </div>
-				<br>
-				<div class="overlay-href"><a class="overlay-href" href="mailto:gmastrbit@gmail.com" target="_blank">gmastrbit@gmail.com</a></div>
+				<a href="mailto:gmastrbit@gmail.com" class="ico gradient1" data-gradient-css="linear-gradient(45deg, #9575cd, #7e57c2, #673ab7, #5e35b1, #512da8, #4527a0, #311b92)"> G </a> 
 			</div>
 			<script src="mypage/js/jquery.min.js"></script>
-			<script src="mypage/js/ripple.js"></script>
 			<div class="menu">
 				<nav class="menu menu--ferdinand">
 					<ul class="menu__list js-scrollspy-nav" id="top-menu">
-						<li class="menu__item menu__item--current">
-							<a href="#my-head" class="menu__link">Головне</a>
-						</li>
-						<li class="menu__item">
-							<a href="#my-projects" id="ss2" class="menu__link">Проекти</a>
-						</li>
-						<li class="menu__item">
-							<a href="#my-contacts" class="menu__link">Контакти</a>
-						</li>
-						<li class="menu__item">
-							<a href="#my-message" class="menu__link">Напишіть мені</a>
-						</li>
+						<li class="menu__item menu__item--current">	<a href="#my-head" class="menu__link">Головне</a> </li>
+						<li class="menu__item"> <a href="#my-projects" id="ss2" class="menu__link">Проекти</a> </li>
+						<li class="menu__item"> <a href="#my-contacts" class="menu__link">Контакти</a> </li>
+						<li class="menu__item"> <a href="#my-message" class="menu__link">Напишіть мені</a> </li>
 					</ul>
 				</nav>
 			</div>
@@ -167,16 +145,12 @@
 						<div class="my_image">
 							<i class="fa fa-spinner fa-pulse fa-3x fa-fw spinner"></i>
 						</div>
-						<a href="https://www.instagram.com/p/BU6N6O5AAZV/?taken-by=gmastrbit" target="_blank"> 
-							<!-- <img class="lazy" src="mypage/img/pixel.png" data-original="mypage/img/image.jpg" width="250" height="250" alt="I am"> -->
+						<a href="https://www.instagram.com/p/BU6N6O5AAZV/?taken-by=gmastrbit" target="_blank"> 							
 							<img src="mypage/img/image.jpg" width="250" height="250" alt="I am">
 						</a>
 					</div>
 					<div class="mytext">
-						Богдан Щербаков <br>
-						Мені 19 років <br>
-						Захоплююсь дизайном <br>
-						Навики: html, css, php, js <br>
+						Богдан Щербаков <br> Мені 19 років <br> Захоплююсь програмуванням, дизайном <br> Навики: HTML, CSS, PHP, JS, SQL, C++<br>
 						<div class="only-mob"><br><br></div>
 					</div>
 				</div>
@@ -185,9 +159,7 @@
 		</div>
 		<div class="projects-mob"> </div>
 		<div class="projects">
-			<div class="hello wow fadeIn animated">
-				Проекти
-			</div>
+			<div class="hello wow fadeIn animated"> Проекти	</div>
 			<br><br><br><br><br><br>
 			<div class="project">
 				<div class="project-image">
@@ -198,9 +170,7 @@
 					<img src="mypage/img/project_1/project_image_1.png" class="lazy project-img-mobile"  alt="TBEC">
 				</div>
 				<div class="project-info wow fadeIn animated">
-					<div class="project-header">
-						Презентація коледжу
-					</div>
+					<div class="project-header"> Презентація коледжу </div>
 					<div class="project-text">
 						Виконаний в стилі презентації сайт, який розповідає і показує про ТБЕК різноманітну інформацію, статистику та інше.
 						<div class="ots"></div>
@@ -219,7 +189,7 @@
 						</filter>
 					</defs>
 				</svg>
-				<button id="component-1" class="button button--1 wow fadeIn animated" onclick="window.open('https://hwork.net/tbec/', '_blank')">
+				<button id="component-1" class="button button--1 wow bounceInUp animated" onclick="window.open('https://hwork.net/tbec/', '_blank')">
 					Перейти <i class="fa fa-arrow-right" aria-hidden="true"></i>
 					<span class="button__container">
 						<span class="circle top-left"></span>
@@ -244,9 +214,7 @@
 					<img src="mypage/img/project_2/project_image_2.png" class="lazy project-img-mobile"  alt="My profile">
 				</div>
 				<div class="project-info wow fadeIn animated">
-					<div class="project-header">
-						Мій профіль
-					</div>
+					<div class="project-header"> Мій профіль </div>
 					<div class="project-text">
 						Натхненний дизайном iOS, створив сторінку про себе та свої творіння часто використовуючи ефект розмиття для різних елементів.
 						<div class="ots"></div>
@@ -265,7 +233,7 @@
 						</filter>
 					</defs>
 				</svg>
-				<button id="component-2" class="button button--2 wow fadeIn animated" onclick="window.open('https://hwork.net/profile/', '_blank')">
+				<button id="component-2" class="button button--2 wow bounceInUp animated" onclick="window.open('https://hwork.net/profile/', '_blank')">
 					Перейти <i class="fa fa-arrow-right" aria-hidden="true"></i>
 					<span class="button__container2">
 						<span class="circle2 top-left2"></span>
@@ -290,9 +258,7 @@
 					<img src="mypage/img/project_3/project_image_3.png" class="lazy project-img-mobile"  alt="PS-46">
 				</div>
 				<div class="project-info wow fadeIn animated">
-					<div class="project-header">
-						Сайт для студентів
-					</div>
+					<div class="project-header"> Сайт для студентів	</div>
 					<div class="project-text">
 						На сайті є все необхідне для студента: зміни в розкладі, графік дзвінків, домашнє завдання та інша різноманітна інформація.
 						<div class="ots"></div>
@@ -311,7 +277,7 @@
 					</filter>
 					</defs>
 				</svg>
-				<button id="component-3" class="button button--3 wow fadeIn animated" onclick="window.open('https://hwork.net/ps/', '_blank')">
+				<button id="component-3" class="button button--3 wow bounceInUp animated" onclick="window.open('https://hwork.net/ps/', '_blank')">
 					Перейти <i class="fa fa-arrow-right" aria-hidden="true"></i>
 					<span class="button__container3">
 						<span class="circle3 top-left3"></span>
@@ -336,9 +302,7 @@
 					<img src="mypage/img/project_4/project_image_4.png" class="lazy project-img-mobile"  alt="Fupl">
 				</div>
 				<div class="project-info wow fadeIn animated">
-					<div class="project-header">
-						Власний файлообмінник
-					</div>
+					<div class="project-header"> Власний файлообмінник </div>
 					<div class="project-text">
 						Дозволяє завантажити файл, для того, аби потім можна було зручно ним поділитися чи використовувати в своїх цілях будь-де і будь-коли. 
 						<div class="ots"></div>
@@ -357,7 +321,7 @@
 						</filter>
 					</defs>
 				</svg>
-				<button id="component-4" class="button button--4 wow fadeIn animated" onclick="window.open('https://hwork.net/files/', '_blank')">
+				<button id="component-4" class="button button--4 wow bounceInUp animated" onclick="window.open('https://hwork.net/files/', '_blank')">
 					Перейти <i class="fa fa-arrow-right" aria-hidden="true"></i>
 					<span class="button__container4">
 						<span class="circle4 top-left4"></span>
@@ -382,9 +346,7 @@
 					<img src="mypage/img/project_5/project_image_5.png" class="lazy project-img-mobile"  alt="Secta">
 				</div>
 				<div class="project-info wow fadeIn animated">
-					<div class="project-header">
-						Соцмережа для об'єднання
-					</div>
+					<div class="project-header"> Соцмережа для об'єднання </div>
 					<div class="project-text">
 						Тут можна створити профіль, ділитися творчістю, спілкуватися з іншими учасниками.
 						<div class="ots"></div>
@@ -403,7 +365,7 @@
 						</filter>
 					</defs>
 				</svg>
-				<button id="component-5" class="button button--5 wow fadeIn animated" onclick="window.open('https://hwork.net/secta/', '_blank')">
+				<button id="component-5" class="button button--5 wow bounceInUp animated" onclick="window.open('https://hwork.net/secta/', '_blank')">
 					Перейти <i class="fa fa-arrow-right" aria-hidden="true"></i>
 					<span class="button__container5">
 						<span class="circle5 top-left5"></span>
@@ -428,9 +390,7 @@
 					<img src="mypage/img/project_6/project_image_6.png" class="lazy project-img-mobile"  alt="Kazka">
 				</div>
 				<div class="project-info wow fadeIn animated">
-					<div class="project-header">
-						Сайт вихователя
-					</div>
+					<div class="project-header"> Сайт вихователя </div>
 					<div class="project-text">
 						Створений в стилі Material Design сайт для того, щоб продемонструвати багаторічну працю викладача.
 						<div class="ots"></div>
@@ -449,7 +409,7 @@
 						</filter>
 					</defs>
 				</svg>
-				<button id="component-6" class="button button--6 wow fadeIn animated" onclick="window.open('http://kazka8.esy.es/', '_blank')">
+				<button id="component-6" class="button button--6 wow bounceInUp animated" onclick="window.open('http://kazka8.esy.es/', '_blank')">
 					Перейти <i class="fa fa-arrow-right" aria-hidden="true"></i>
 					<span class="button__container6">
 						<span class="circle6 top-left6"></span>
@@ -474,9 +434,7 @@
 					<img src="mypage/img/project_7/project_image_7.png" class="lazy project-img-mobile"  alt="Okta">
 				</div>
 				<div class="project-info wow fadeIn animated">
-					<div class="project-header">
-						Сторінка-лендінг
-					</div>
+					<div class="project-header">Сторінка-лендінг </div>
 					<div class="project-text">
 						Перші спроби створити landing page власними руками. На сторінці є перелік товарів, їх представлення, опис і т.д.
 						<div class="ots"></div>
@@ -495,7 +453,7 @@
 						</filter>
 					</defs>
 				</svg>
-				<button id="component-7" class="button button--7 wow fadeIn animated" onclick="window.open('http://konstruktor-okta.esy.es/', '_blank')">
+				<button id="component-7" class="button button--7 wow bounceInUp animated" onclick="window.open('http://konstruktor-okta.esy.es/', '_blank')">
 					Перейти <i class="fa fa-arrow-right" aria-hidden="true"></i>
 					<span class="button__container7">
 						<span class="circle7 top-left7"></span>
@@ -520,11 +478,9 @@
 					<img src="mypage/img/project_8/project_image_8.png" class="lazy project-img-mobile"  alt="Welcome to the jungle">
 				</div>
 				<div class="project-info wow fadeIn animated">
-					<div class="project-header">
-						Улюблений трек
-					</div>
+					<div class="project-header"> Улюблений трек	</div>
 					<div class="project-text">
-						Знайомство з html5-плеєром переросло у фан-сторінку треку вісімдесятих років.
+						Знайомство з HTML5-плеєром переросло у фан-сторінку треку вісімдесятих років.
 						<div class="ots"></div>
 						На сторінці є кнопки керування плеєром, слова та відеокліп.
 					</div>
@@ -541,7 +497,7 @@
 						</filter>
 					</defs>
 				</svg>
-				<button id="component-8" class="button button--8 wow fadeIn animated" onclick="window.open('https://hwork.net/only/', '_blank')">
+				<button id="component-8" class="button button--8 wow bounceInUp animated" onclick="window.open('https://hwork.net/only/', '_blank')">
 					Перейти <i class="fa fa-arrow-right" aria-hidden="true"></i>
 					<span class="button__container8">
 						<span class="circle8 top-left8"></span>
@@ -566,9 +522,7 @@
 					<img src="mypage/img/project_10/project_image_10.png" class="lazy project-img-mobile"  alt="Mailer">
 				</div>
 				<div class="project-info wow fadeIn animated">
-					<div class="project-header">
-						Надсилач повідомлень
-					</div>
+					<div class="project-header"> Надсилач повідомлень </div>
 					<div class="project-text">
 						Тепер ви можете анонімно або відкрито надіслати будь-яке повідомлення на будь-яку email адресу.
 						<div class="ots"></div>
@@ -587,7 +541,7 @@
 						</filter>
 					</defs>
 				</svg>
-				<button id="component-9" class="button button--9 wow fadeIn animated" onclick="window.open('https://hwork.net/mail/', '_blank')">
+				<button id="component-9" class="button button--9 wow bounceInUp animated" onclick="window.open('https://hwork.net/mail/', '_blank')">
 					Перейти <i class="fa fa-arrow-right" aria-hidden="true"></i>
 					<span class="button__container9">
 						<span class="circle9 top-left9"></span>
@@ -604,9 +558,7 @@
 		</div>
 		<div class="bookmarks">
 			<br><br><br><br>
-			<div class="hello wow fadeIn animated">
-				Концепція
-			</div>
+			<div class="hello wow fadeIn animated">	Концепція </div>
 			<br><br><br><br><br><br><br>
 			<div class="bmark1">
 				<div class="bmark-image wow fadeIn animated zoom">
@@ -624,9 +576,7 @@
 					</div>
 				</div>
 				<div class="project-info wow fadeIn animated">
-					<div class="project-header">
-						Розмиття
-					</div>
+					<div class="project-header"> Розмиття </div>
 					<div class="project-text">
 						Використайте цей прийом для того, аби спрямувати увагу на текст і мати при цьому гарний фон. Не треба забувати, що можна використовувати розмиття не лише для фону, а й для окремих елементів. Бажано застосовувати і з іншими прийомами. 
 					</div>
@@ -647,9 +597,7 @@
 					</div>
 				</div>
 				<div class="project-info wow fadeIn animated">
-					<div class="project-header">
-						Градієнт
-					</div>
+					<div class="project-header"> Градієнт </div>
 					<div class="project-text">
 						Популярний нині прийом може урізноманітнити кольорову гаму сайта, привернути увагу користувача. Як радіальний так і лінійний градієнти можна використовувати як елементи фону сторінки або ж простої кнопки. 
 					</div>
@@ -698,9 +646,7 @@
 					</div>
 				</div>
 				<div class="project-info wow fadeIn animated">
-					<div class="project-header">
-						Прозорість
-					</div>
+					<div class="project-header"> Прозорість	</div>
 					<div class="project-text">
 						За допомогою цього прийому можна створити цікаві місця для тексту, чи сфокосувати користувача на якомусь елементі. Можна використовувати для великих і малих елементів і експериментувати з рівнями прозорості.
 					</div>
@@ -713,9 +659,7 @@
 			</div>
 		</div>
 		<div class="contacts">
-			<div class="hello wow fadeIn animated">
-				Контакти
-			</div>
+			<div class="hello wow fadeIn animated">	Контакти </div>
 			<br><br><br><br><br><br>
 			<div class="contacts-box">
 				<div class="dummy dummy-menu wow fadeIn animated">
@@ -783,6 +727,19 @@
 						</li>
 					</ul>
 				</div>
+				<div class="dummy dummy-menu wow fadeIn animated">
+					<ul>
+						<li>
+							<a class="tooltip" href="https://github.com/gmastrbit" target="_blank">
+							<i class="fa fa-github" aria-hidden="true"></i>
+							<span class="tooltip-content"> <span class="tooltip-text"> <span class="tooltip-inner">
+								Профіль в GitHub
+								</span> </span> 
+							</span>
+						</a>
+						</li>
+					</ul>
+				</div>
 			</div> 
 		</div>
 		<div class="mess">
@@ -792,9 +749,7 @@
 			</div>
 		</div>
 		<div class="write-me">
-			<div class="hello wow fadeIn animated">
-				Напишіть мені
-			</div>
+			<div class="hello wow fadeIn animated">	Напишіть мені </div>
 			<br><br><br><br>
 			<div class="feedback">
 				<form method="post" action="sendMessage.php"  class="feedbck">
@@ -818,7 +773,7 @@
 								</filter>
 							</defs>
 							</svg>
-							<button id="send" class="button button--10 wow fadeIn animated component-10" type="submit" name="button">
+							<button id="send" class="button button--10 wow bounceInUp animated component-10" type="submit" name="button">
 								Надіслати  <i class="fa fa-paper-plane" aria-hidden="true"></i>
 								<span class="button__container10">
 									<span class="circle10 top-left10"></span>
@@ -843,16 +798,13 @@
 	</div>
 	<script>
 	$(window).load(function () {
-		$(".load-container").delay(100).fadeOut(500);
+		$(".load-container").delay(100).fadeOut(750);
 	});
 	$(window).scroll(function() { 
 		$('.header').toggleClass('header-shadow', $(this).scrollTop() > 10); 
 	});
 	$(document).ready(function() {
-		$("head").append("<link rel='stylesheet' type='text/css' href='mypage/css/animate.min.css'>");
-		$("head").append("<link rel='stylesheet' type='text/css' href='mypage/css/normalize.css'>");
 		$("head").append("<link rel='stylesheet' type='text/css' href='mypage/css/component.css'>");
-		$("head").append("<link rel='stylesheet' type='text/css' href='mypage/css/zoom.css'>");
 	})
 	$('a[href^="#"]').bind('click.smoothscroll',function (e) {
 		e.preventDefault();
